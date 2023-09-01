cb319b2aa1a1600d34f2a2568755d5e20179d72e
juanacevedo68061 <111782315+juanacevedo68061@users.noreply.github.com>
2023-09-01 19:01:46 -0400

    Quitando campos del docstring models.py

--- administracion/models.py
+++ administracion/models.py
@@ -5,9 +5,10 @@ from django.core.exceptions import ValidationError
 class Categoria(models.Model):
     """
     La clase categoria contiene el campo nombre y contiene un campo booleano que indica si la misma es moderada o no
+    """
+    
     nombre = models.CharField(max_length=100)
     moderada = models.BooleanField(default=True)
-    """
     
     def __str__(self):
         return self.nombre

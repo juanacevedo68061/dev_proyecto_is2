46a6058738dfe6ec9b19e4b7d5cd45cd880ce57b
Juan Manuel <juan.acevedo68061@fpuna.edu.py>
2023-09-03 18:09:43 -0400

    Modificando para que el nombre sea unico

--- administracion/models.py
+++ administracion/models.py
@@ -4,12 +4,20 @@ from django.core.exceptions import ValidationError
 
 class Categoria(models.Model):
     """
-    Clase nueva
+    Modelo que representa una categoría para las publicaciones.
+
+    Atributos:
+        nombre (str): El nombre único de la categoría.
+        moderada (bool): Indica si la categoría está moderada o no.
+
+    Métodos:
+        __str__(): Devuelve una representación de cadena del objeto, que es el nombre de la categoría.
+
     """
-    
-    nombre = models.CharField(max_length=100)
+
+    nombre = models.CharField(max_length=100, unique=True)
     moderada = models.BooleanField(default=True)
-    
+
     def __str__(self):
         return self.nombre
 

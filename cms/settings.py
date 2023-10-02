75341ecaf6779993b414bba3ae81ad29db1f8780
Juan Manuel <juan.acevedo68061@fpuna.edu.py>
2023-10-02 13:53:22 -0300

    actualizado

--- cms/settings.py
+++ cms/settings.py
@@ -69,12 +69,6 @@ STATICFILES_DIRS = [
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 
 
 

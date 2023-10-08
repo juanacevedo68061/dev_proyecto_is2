62b0e8cc65ebb6230c76de9b9d403791c0cf66af
Juan Manuel <juan.acevedo68061@fpuna.edu.py>
2023-10-08 14:59:09 -0300

    cada vez que se ejecuta migrate se podra actualizar la tabla

--- login/signals.py
+++ login/signals.py
@@ -1,7 +1,21 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_migrate, pre_save, post_migrate
+from django.core.signals import request_started
 from django.dispatch import receiver
 from roles.models import Rol
 from .models import Usuario
+from cms.names import actualizar_nombres_vistas  # Importa la función
+
+@receiver(post_migrate)
+def ejecutar_actualizacion(sender, **kwargs):
+    """
+    Ejecuta actualizar_nombres_vistas antes de crear un nuevo usuario.
+
+    Parámetros:
+        sender: Modelo que envía la señal (Usuario).
+        instance: Instancia del usuario que se está a punto de crear.
+    """
+    # Llama a la función para actualizar los nombres de las vistas antes del registro
+    actualizar_nombres_vistas()
 
 @receiver(post_save, sender=Usuario)
 def asignar_roles(sender, instance, created, **kwargs):

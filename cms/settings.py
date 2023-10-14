cb14632325e7a9627f634b62584d554d89af1eaf
Juan Manuel <juan.acevedo68061@fpuna.edu.py>
2023-10-14 11:43:50 -0300

    gmail propio del proyecto

--- cms/settings.py
+++ cms/settings.py
@@ -24,8 +24,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'juan.acevedo68061@gmail.com'  # Tu dirección de correo de Gmail
-EMAIL_HOST_PASSWORD = 'rebk rysq pevu wewy'  # Tu contraseña de correo de Gmail (o usa variables de entorno)
+EMAIL_HOST_USER = 'cms.team.is2@gmail.com'  # Tu dirección de correo de Gmail
+EMAIL_HOST_PASSWORD = 'vseb krgh vskk zdch'  # Tu contraseña de correo de Gmail (o usa variables de entorno)
 
 # Application definition
 

180c8304fd8e8f7c6455654c0dbe57ed4cf5c2e7
AngeloNUNEZ <angeloarielnunezenciso@fpuna.edu.py>
2023-10-31 21:20:27 -0300

    Se agrega test de models.py

---
+++ kanban/test/tests_models.py
@@ -0,0 +1,36 @@
+from django.test import TestCase
+from .models import Registro
+from login.models import Usuario
+from roles.models import Rol
+
+class RegistroModelTest(TestCase):
+
+    def setUp(self):
+        # Creamos una instancia de Usuario y Rol para usarla en el test
+        self.usuario = Usuario.objects.create(username='testuser', password='testpassword123')
+        self.rol = Rol.objects.create(nombre='testrol')
+
+    def test_registro_creation(self):
+        # Creamos una instancia de Registro
+        registro = Registro.objects.create(
+            publicacion_titulo='Test Publicacion',
+            responsable=self.usuario
+        )
+        registro.roles.add(self.rol)
+
+        # Verificamos que se haya creado correctamente
+        self.assertEqual(Registro.objects.count(), 1)
+        self.assertEqual(registro.publicacion_titulo, 'Test Publicacion')
+        self.assertEqual(registro.responsable, self.usuario)
+        self.assertIn(self.rol, registro.roles.all())
+        self.assertIsNotNone(registro.fecha_cambio)
+
+    def test_str_method(self):
+        # Creamos una instancia de Registro
+        registro = Registro.objects.create(
+            publicacion_titulo='Test Publicacion',
+            responsable=self.usuario
+        )
+        # Verificamos el método __str__
+        self.assertEqual(str(registro), f'Registro de Test Publicacion por {self.usuario}')
+

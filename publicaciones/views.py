3f0d1557e43daabdbafe3fd84cc28140f6f480aa
RamosDer <der.ramos93@gmail.com>
2023-11-02 16:41:41 -0300

    modelos y vistas básicos para mostrar el formulario en publicacion

--- publicaciones/views.py
+++ publicaciones/views.py
@@ -28,7 +28,9 @@ from roles.decorators import permiso_requerido, permiso_redireccion_requerido
 import json
 from django.http import HttpResponseBadRequest
 from django.views.decorators.http import require_http_methods
-
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+from django_comments_xtd.models import XtdComment
 
 @permiso_requerido
 @login_required
@@ -489,3 +491,14 @@ def calificar(request, publicacion_id):
         return JsonResponse({'rating': rating, 'calificaciones': cantidad})
 
     return HttpResponseBadRequest('Método no permitido')
+
+@receiver(post_save, sender=XtdComment)
+def update_comment_count(sender, instance, created, **kwargs):
+    if created:
+        instance.content_object.comments += 1
+        instance.content_object.save()
+
+@receiver(post_delete, sender=XtdComment)
+def decrease_comment_count(sender, instance, **kwargs):
+    instance.content_object.comments -= 1
+    instance.content_object.save()

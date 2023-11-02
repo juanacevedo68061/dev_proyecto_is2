5049a0d12d63fc0b3850046b36e7101ca6763b56
Juan Manuel <juan.acevedo68061@fpuna.edu.py>
2023-11-02 17:34:04 -0300

    poblacion de datos lo hace el docker-compose

--- deployment.py
+++ deployment.py
@@ -1,7 +1,69 @@
 import os
+import sys
+import subprocess
+import threading
 
-# Poblar la base de datos
-os.system("python poblacion/poblar.py")
+def has_uncommitted_changes():
+    try:
+        result = os.system("git diff-index --quiet HEAD --")
+        return result != 0
+    except Exception as e:
+        print(f"Error al verificar cambios sin commitear: {str(e)}")
+        sys.exit(1)
 
-# Iniciar el servidor de desarrollo de Django
-os.system("python manage.py runserver")
+def test_project():
+    try:
+        def test_server():
+            subprocess.run(["python", "manage.py", "runserver"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+        test_thread = threading.Thread(target=test_server)
+        test_thread.start()
+        test_thread.join(timeout=10)  # Espera hasta 10 segundos, ajusta el tiempo según tus necesidades
+
+        if test_thread.is_alive():
+            test_thread.join()  # Si el servidor aún se está ejecutando después del tiempo de espera, lo detenemos
+            return False
+        else:
+            return True
+
+    except Exception as e:
+        print(f"Error al probar el proyecto: {str(e)}")
+        return False
+
+def commit_changes(commit_message):
+    os.system("git add .")
+    os.system(f'git commit -m "{commit_message}"')
+
+def change_branch(branch_name):
+    if has_uncommitted_changes():
+        if test_project():
+            commit_message = input("Por favor, ingresa un mensaje de commit para tus cambios: ")
+            commit_changes(commit_message)
+            print("Cambios commiteados correctamente.")
+        else:
+            print("Los cambios generan inconvenientes. No se pueden commitear ni cambiar de rama.")
+            sys.exit(1)
+
+    if test_project():
+        print("El proyecto se sirve de forma adecuada.")
+        print("¿Deseas cambiar de rama? (Sí/No): ")
+        user_input = input()
+        if user_input.lower() == "si" or user_input.lower() == "sí":
+            try:
+                os.system(f"git checkout {branch_name}")
+                print(f"Cambiado a la rama {branch_name}")
+            except Exception as e:
+                print(f"Error al cambiar a la rama {branch_name}: {str(e)}")
+                sys.exit(1)
+        else:
+            print("Cambios commiteados pero no se ha cambiado de rama.")
+    else:
+        print("El proyecto no se sirve de forma adecuada. Por favor, corrige los errores antes de cambiar de rama.")
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("Uso: python deployment.py <branch_name>")
+        sys.exit(1)
+
+    branch_name = sys.argv[1]
+    change_branch(branch_name)

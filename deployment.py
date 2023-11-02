61d23bd83267226128993e9f3d3a67e25bc1f388
Juan Manuel <juan.acevedo68061@fpuna.edu.py>
2023-11-02 19:15:28 -0300

    nuevo deployd

--- deployment.py
+++ deployment.py
@@ -18,10 +18,10 @@ def test_project():
 
         test_thread = threading.Thread(target=test_server)
         test_thread.start()
-        test_thread.join(timeout=10)  # Espera hasta 10 segundos, ajusta el tiempo según tus necesidades
+        test_thread.join(timeout=10)
 
         if test_thread.is_alive():
-            test_thread.join()  # Si el servidor aún se está ejecutando después del tiempo de espera, lo detenemos
+            test_thread.join()
             return False
         else:
             return True
@@ -40,12 +40,9 @@ def change_branch(branch_name):
             commit_message = input("Por favor, ingresa un mensaje de commit para tus cambios: ")
             commit_changes(commit_message)
             print("Cambios commiteados correctamente.")
-
-    if test_project():
-        print("El proyecto se sirve de forma adecuada.")
-        if has_uncommitted_changes():
-            print("¿Deseas cambiar de rama? (Sí/No): ")
-            user_input = input()
+            
+            print("Aviso: El proyecto se sirve de manera adecuada, pero aún es propenso a errores en el lado del cliente.")
+            user_input = input("¿Deseas continuar con el despliegue? (Sí/No): ")
             if user_input.lower() == "si" or user_input.lower() == "sí":
                 try:
                     os.system(f"git checkout {branch_name}")
@@ -54,16 +51,16 @@ def change_branch(branch_name):
                     print(f"Error al cambiar a la rama {branch_name}: {str(e)}")
                     sys.exit(1)
             else:
-                print("Cambios commiteados pero no se ha cambiado de rama.")
+                print("No se ha cambiado de rama. Proceso de deployment.py finalizado.")
         else:
-            try:
-                os.system(f"git checkout {branch_name}")
-                print(f"Cambiado a la rama {branch_name}")
-            except Exception as e:
-                print(f"Error al cambiar a la rama {branch_name}: {str(e)}")
-                sys.exit(1)
+            print("Error: Los cambios generan errores en el proyecto. El proyecto no se sirve adecuadamente. Proceso de deployment.py finalizado.")
     else:
-        print("El proyecto no se sirve de forma adecuada. Por favor, corrige los errores antes de cambiar de rama.")
+        try:
+            os.system(f"git checkout {branch_name}")
+            print(f"Cambiado a la rama {branch_name}")
+        except Exception as e:
+            print(f"Error al cambiar a la rama {branch_name}: {str(e)}")
+            sys.exit(1)
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:

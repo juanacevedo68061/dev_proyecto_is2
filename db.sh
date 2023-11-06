f24a0719efe0bcb8b0cf56f5cec4eee43d737709
Juan Manuel <juan.acevedo68061@fpuna.edu.py>
2023-11-06 20:49:23 -0300

    reforma

--- db.sh
+++ db.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-./inicio.sh
-unzip -o credencial.zip
 python manage.py makemigrations roles login kanban administracion publicaciones
 python manage.py migrate
 python poblacion/poblar.py
-gunicorn cms.wsgi:application --bind 0.0.0.0:8000

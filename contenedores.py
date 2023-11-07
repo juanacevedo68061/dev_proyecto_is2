b0b709d13b99b395cf70f20e96abe7a9a8346b33
Juan Manuel <juan.acevedo68061@fpuna.edu.py>
2023-11-07 12:34:41 -0300

    Aqui vamos por la ultima esperanza

--- contenedores.py
+++ contenedores.py
@@ -1,8 +1,18 @@
 import subprocess
+import sys
 
 def run_docker_compose(file):
     command = ["docker-compose", "-f", file, "up", "-d"]
     subprocess.run(command, check=True)
+
+def run_second_docker_compose():
+    command = ["docker-compose", "-f", "docker-compose.yml", "up", "-d"]
+    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
+
+    for line in process.stdout:
+        sys.stdout.write(line)
+
+    process.wait() 
 
 def monitor_logs_and_execute_second_docker_compose(container_name):
     first_message = "PostgreSQL init process complete; ready for start up."
@@ -13,9 +23,9 @@ def monitor_logs_and_execute_second_docker_compose(container_name):
 
         if first_message in line:
             print("\n\nEMPIEZA EL SEGUNDO DOCKER-COMPOSE")
-            run_docker_compose("docker-compose.yml")
+            run_second_docker_compose()
             print("\n\nSE EJECUTÓ EL SEGUNDO DOCKER-COMPOSE")
-        
+
 if __name__ == "__main__":
     container_name = "proyecto_is2-db-1"
 

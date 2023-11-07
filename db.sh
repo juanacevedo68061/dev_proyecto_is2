6861de89c15cb39ecd2838667dfa9ec07bee19ae
Juan Manuel <juan.acevedo68061@fpuna.edu.py>
2023-11-06 21:58:06 -0300

    nuevo

--- db.sh
+++ db.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+until pg_isready -h db -p 5432 -U postgres
+do
+    echo "Esperando a que la base de datos esté disponible..."
+    sleep 2
+done &
+
 python manage.py makemigrations roles login kanban administracion publicaciones
 python manage.py migrate
 python poblacion/poblar.py

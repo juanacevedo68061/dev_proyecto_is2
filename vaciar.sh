7d870c3f7a7622dd8eba56b1873a516a7fb2069c
Juan Manuel <juan.acevedo68061@fpuna.edu.py>
2023-11-07 11:10:03 -0300

    nuevo vaciar

--- vaciar.sh
+++ vaciar.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
 
-docker-compose down
-docker rmi $(docker images)
-docker volume rm $(docker volume ls -q)
+# Detener y eliminar todos los contenedores en ejecución
+docker stop $(docker ps -q)
+docker rm -f $(docker ps -a -q)
+
+# Detener y eliminar todos los servicios de Docker Compose
+docker-compose down --volumes
+
+# Eliminar todas las imágenes de Docker
+docker rmi $(docker images -q)
+
+# Eliminar todos los volúmenes Docker
+docker volume prune --force
+
+# Eliminar todas las redes Docker de manera forzada
 docker network prune --force

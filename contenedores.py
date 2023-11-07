c2f94f1250c0270c9921da4c0a57cf879eb3c1f4
Juan Manuel <juan.acevedo68061@fpuna.edu.py>
2023-11-07 10:37:54 -0300

    cambiando de enfoque

---
+++ contenedores.py
@@ -0,0 +1,30 @@
+import subprocess
+import os
+
+def run_docker_compose(file):
+    command = f"docker-compose -f {file} up -d"
+    os.system(command)
+
+def monitor_logs_and_execute_second_docker_compose(container_name):
+    first_message = "CREATE DATABASE"
+    second_message = "database system is ready to accept connections"
+
+    state = 0
+
+    process = subprocess.Popen(["docker", "logs", "--follow", container_name], stdout=subprocess.PIPE, text=True)
+
+    for line in process.stdout:
+        print(line, end="")
+
+        if state == 0 and first_message in line:
+            state = 1
+        elif state == 1 and second_message in line:
+            run_docker_compose("docker-compose.yml")
+            break
+
+if __name__ == "__main__":
+    container_name = "proyecto_is2-db-1"
+
+    run_docker_compose("db.yml")
+
+    monitor_logs_and_execute_second_docker_compose(container_name)  # Monitorea los logs y ejecuta el segundo archivo

c6de2b74bf2e9cd9c75c8b0ed0e0787dcd417d97
Juan Manuel <juan.acevedo68061@fpuna.edu.py>
2023-11-07 12:58:20 -0300

    Aqui vamos por la ultima esperanza

--- contenedores.py
+++ contenedores.py
@@ -8,14 +8,18 @@ def monitor_logs_and_execute_second_docker_compose(container_name):
     first_message = "PostgreSQL init process complete; ready for start up."
     process = subprocess.Popen(["docker", "logs", "--follow", container_name], stdout=subprocess.PIPE, text=True)
 
+    second_docker_logs = open("second_docker_compose.log", "w")
+
     for line in process.stdout:
         print("contenedores = ", line, end="")
+        second_docker_logs.write(line)  # Guarda los logs en un archivo
 
         if first_message in line:
             print("\n\nEMPIEZA EL SEGUNDO DOCKER-COMPOSE")
             run_docker_compose("docker-compose.yml")
             print("\n\nSE EJECUTÓ EL SEGUNDO DOCKER-COMPOSE")
-            process.wait()  # Esperar a que el proceso del segundo Docker Compose termine
+
+    second_docker_logs.close()
 
 if __name__ == "__main__":
     container_name = "proyecto_is2-db-1"

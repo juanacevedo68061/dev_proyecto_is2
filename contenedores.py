cb14a11dd40afa60118c3b81b940fadb3feb9e97
Juan Manuel <juan.acevedo68061@fpuna.edu.py>
2023-11-07 13:42:15 -0300

    probemos

--- contenedores.py
+++ contenedores.py
@@ -10,7 +10,7 @@ def monitor_logs_and_execute_second_docker_compose(container_name):
     received_first_message = False
     log_file = "container_logs.txt"
 
-    process = subprocess.Popen(["docker", "logs", "--follow", "--tail", "all", container_name], stdout=subprocess.PIPE, text=True)
+    process = subprocess.Popen(["docker", "logs", "--follow", "--tail", "all", container_name], stdout=subprocess.PIPE, text=True, bufsize=1, universal_newlines=True)
 
     with open(log_file, "w") as log_output:
         for line in process.stdout:
@@ -22,8 +22,10 @@ def monitor_logs_and_execute_second_docker_compose(container_name):
                 received_first_message = True
                 print("\n\nDETECTOOO")
 
-            if received_first_message and second_message in line:
-                print("\n\nSE EJECUTÓ EL SEGUNDO DOCKER-COMPOSE")
+    process.wait()  # Esperar a que el proceso termine para imprimir la última línea
+
+    if received_first_message:
+        print(f"\n\nSE EJECUTÓ EL SEGUNDO DOCKER-COMPOSE\n\nÚltima línea de log: {line}")
 
 if __name__ == "__main__":
     container_name = "proyecto_is2-db-1"

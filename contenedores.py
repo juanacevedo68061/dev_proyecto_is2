4eeed7a9e3f922c7fa4255f5af10f1d068ad32ab
Juan Manuel <juan.acevedo68061@fpuna.edu.py>
2023-11-07 13:30:45 -0300

    quitando el puto break de mierda que no quite

--- contenedores.py
+++ contenedores.py
@@ -6,26 +6,27 @@ def run_docker_compose(file):
 
 def monitor_logs_and_execute_second_docker_compose(container_name):
     first_message = "PostgreSQL init process complete; ready for start up."
+    second_message = "database system is ready to accept connections"
+    received_first_message = False
+    log_file = "container_logs.txt"
+
     process = subprocess.Popen(["docker", "logs", "--follow", container_name], stdout=subprocess.PIPE, text=True)
 
-    second_docker_compose_started = False
-
-    for line in process.stdout:
-        print("contenedores = ", line, end="")
-
-        if first_message in line:
-            print("\n\nEMPIEZA EL SEGUNDO DOCKER-COMPOSE")
-            run_docker_compose("docker-compose.yml")
-            second_docker_compose_started = True
-            print("\n\nSE EJECUTÓ EL SEGUNDO DOCKER-COMPOSE")
-
-        # Agregar una condición para seguir mostrando los logs del segundo Docker Compose.
-        if second_docker_compose_started:
+    with open(log_file, "w") as log_output:
+        for line in process.stdout:
+            log_output.write(line)
+            log_output.flush()
             print("contenedores = ", line, end="")
+
+            if first_message in line:
+                received_first_message = True
+                print("\n\nDETECTOOO")
+
+            if received_first_message and second_message in line:
+                print("\n\nSE EJECUTÓ EL SEGUNDO DOCKER-COMPOSE")
 
 if __name__ == "__main__":
     container_name = "proyecto_is2-db-1"
 
     run_docker_compose("db.yml")
-
     monitor_logs_and_execute_second_docker_compose(container_name)

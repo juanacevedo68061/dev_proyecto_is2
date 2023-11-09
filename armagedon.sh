91ee150cf256e8b250507d392dab4bc01a78c2e3
Juan Manuel <juan.acevedo68061@fpuna.edu.py>
2023-11-09 15:28:45 -0300

    actulizando con agujero negro

--- armagedon.sh
+++ armagedon.sh
@@ -1,30 +1,16 @@
 #!/bin/sh
 
 # Detener y eliminar todos los contenedores en ejecución
-if [ "$(docker ps -q)" ]; then
-    docker stop $(docker ps -q)
-    docker rm -f $(docker ps -a -q)
-else
-    echo "No hay contenedores en ejecución para detener o eliminar."
-fi
+docker stop $(docker ps -q) >/dev/null 2>&1
+docker rm -f $(docker ps -a -q) >/dev/null 2>&1
 
 # Eliminar todas las imágenes de Docker
-if [ "$(docker images -q)" ]; then
-    docker rmi $(docker images -q)
-else
-    echo "No hay imágenes de Docker para eliminar."
-fi
+docker rmi -f $(docker images -q) >/dev/null 2>&1
 
 # Eliminar todos los volúmenes Docker
-if [ "$(docker volume ls -q)" ]; then
-    docker volume prune --force
-else
-    echo "No hay volúmenes de Docker para eliminar."
-fi
+docker volume prune --force >/dev/null 2>&1
 
 # Eliminar todas las redes Docker de manera forzada
-if [ "$(docker network ls -q)" ]; then
-    docker network prune --force
-else
-    echo "No hay redes de Docker para eliminar."
-fi
+docker network prune --force >/dev/null 2>&1
+
+echo "Entorno Docker limpiado exitosamente."

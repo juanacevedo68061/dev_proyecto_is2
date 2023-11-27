6e29658d08d2d0b6d490f66f586145d6f368c9ad
Juan Manuel <juan.acevedo68061@fpuna.edu.py>
2023-11-27 12:55:21 -0300

    mostrando comentarios de la publicacion

--- comentarios/forms.py
+++ comentarios/forms.py
@@ -9,5 +9,5 @@ class CommentForm(forms.ModelForm):
             'texto': forms.Textarea(attrs={'rows': 1, 'style': 'border: none; outline: none; resize: none; overflow: hidden;', 'placeholder': '¿Qué piensas?'}),
         }
         labels = {
-            'texto': '',  # Deja el label vacío para que no se muestre
+            'texto': '',
         }
